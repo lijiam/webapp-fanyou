fa9ca29e2adfc30d133dc6eefa00a4743f733a10
lijiam <583586309@qq.com>
2020-01-16 16:47:33 +0800

    第一次提交

---
+++ serve/src/test/java/com/webapp/fanyou/FanyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.fanyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanyouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
